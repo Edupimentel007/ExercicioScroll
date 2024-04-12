f31f421373a6f761021fd1a850f1b55e4625973c
Eduardo Pimentel <125682114+Edupimentel007@users.noreply.github.com>
2024-04-12 14:46:14 -0300

    Exercicio usando Scroll

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, View } from "react-native";
+import { styles } from "./src/styles/Style";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View styles={styles.header}></View>
+      <ScrollView horizontal={true}>
+        <View style={styles.cont}>
+          <View style={[styles.body, { backgroundColor: "blue" }]}></View>
+          <View style={[styles.body, { backgroundColor: "green" }]}></View>
+          <View style={[styles.body, { backgroundColor: "purple" }]}></View>
+          <View style={[styles.body, { backgroundColor: "pink" }]}></View>
+          <View style={[styles.body, { backgroundColor: "gray" }]}></View>
+          <View style={[styles.body, { backgroundColor: "yellow" }]}></View>
+        </View>
+      </ScrollView>
+      <View style={styles.footer}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/Style.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5dc"
+    },
+
+   body: {
+    width: 500,
+    height: 500,
+    borderRadius: 10,
+    paddingVertical: 10,
+   },
+
+   header: {
+    height: 10,
+    backgroundColor: "red",
+    width: 100,
+
+   },
+
+   footer: {
+    width: 150,
+    height: 150,
+  },
+
+   cont: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    },
+
+});
